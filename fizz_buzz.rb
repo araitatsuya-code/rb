ce74a3b03c69f5155302067264a627ac5694db80
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-02-29 20:53:53 +0900

    exp16

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,12 +1,38 @@
 
-for i in 1..30
-  if i % 15 == 0
-    puts "fizz_buzz"
-  elsif i % 3 == 0
-    puts "fizz"  
-  elsif i % 5 == 0
-    puts "buzz"
-  else
-    puts i
+# for i in 1..30
+#   if i % 15 == 0
+#     puts "fizz_buzz"
+#   elsif i % 3 == 0
+#     puts "fizz"  
+#   elsif i % 5 == 0
+#     puts "buzz"
+#   else
+#     puts i
+#   end
+# end
+
+# (1..30).each do |num|
+#   puts case 0
+#     when num % 15 then :FizzBuzz
+#     when num % 3  then :Fizz
+#     when num % 5  then :Buzz
+#     else num
+#   end
+# end
+
+num = 1
+while num < 101
+  str = ""
+  if num % 3 == 0
+    str = str + "fizz"
   end
+
+  if num % 5 == 0
+    str = str + "buzz"
+  end
+  if str == ""
+    str = str + num.to_s
+  end
+  puts str
+  num += 1
 end
