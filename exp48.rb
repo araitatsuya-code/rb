811d420ef666922829baa09c058b66f40fdfcb81
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-03-04 23:19:00 +0900

    exp45~50

---
+++ exp48.rb
@@ -0,0 +1,8 @@
+class Impression < ActiveRecord::Base
+  VALID_YOUTUBE_URL_REGEX = /\A.* YOUTUBE. *\z/
+  validates :url, presence: true, format: { with: VALID_YOUTUBE_URL_REGEX }
+end
+
+# 2行目のvalidationを完成させてください。ただし条件は、
+# 「youtube」という文字が投稿された文字列に含まれていることを確かめることとします。
+
